870a6b1bf31ddd454b04de3ee0eeba681bbba701
Marc Mamaradlo <marc.edwin.mamaradlo@gmail.com>
2022-08-04 09:33:22 +0800

    Initial commit

---
+++ Movies API.js
@@ -0,0 +1,105 @@
+const form = document.querySelector('#form');
+const searchForm = document.querySelector('#searchForm');
+const button = document.querySelector('#button');
+const container = document.querySelector('#container');
+const newDiv = document.createElement('div');
+const title = document.querySelector('#title');
+const summary = document.querySelector('#summary');
+const language = document.querySelector('#language');
+const category = document.querySelector('#category');
+const genre = document.querySelector('#genre');
+const img = document.querySelector('#img')
+
+form.addEventListener('submit', async function (e) {
+  e.preventDefault();
+  const searchTerm = form.elements.query.value;
+  const config = { params: { q: searchTerm } };
+  try {
+    const res = await axios.get(`http://api.tvmaze.com/search/shows`, config);
+    makeImages(res.data);
+  } catch (error) {
+    console.log('error');
+  }
+});
+
+const makeImages = (shows) => {
+  let html = '';
+  shows.map(function (newArr) {
+    let imageElement, bodyCard, listLanguage, listCategory, listGenre;
+
+    try {
+      imageElement = `
+        <img src="${newArr.show.image.medium}" class="card-img-top" alt="..." id="img"/>
+        `
+    } catch (e) {
+      console.log("image not found");
+      imageElement = `
+        <img src="https://images.unsplash.com/photo-1609743522653-52354461eb27?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"/>
+        `
+    }
+    try {
+      bodyCard = `
+        <h5 class="card-title" id="title">${newArr.show.name}</h5>
+      `
+    } catch (e) {
+      console.log("body card error");
+    }
+
+    // try {
+    //   groupFlush = `
+    //     <li class="list-group-item" id="language">${newArr.show.language}</li>
+    //     <li class="list-group-item" id="category">${newArr.show.type}</li>
+    //     <li class="list-group-item" id="genre">${newArr.show.genres}</li>
+    //   `
+    // } catch (e) {
+    //   console.log("groupFlushError");
+    // }
+
+    try {
+      listLanguage = `${newArr.show.language}`
+    } catch (e) {
+      console.log("language not found");
+      listLanguage = `Language:`
+    }
+
+    try {
+      listCategory = `${newArr.show.type}`
+    } catch (e) {
+      console.log("category not found");
+      listCategory = `Category:`
+    }
+
+    try {
+      listGenre = `${newArr.show.genres}`
+    } catch (e) {
+      console.log("genre not found");
+      listGenre = `Genre:`
+    }
+
+    html += `
+            <div class="card">
+              ${imageElement}
+              <div class="card-body">
+                ${bodyCard}
+              </div>
+              <div>
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item" id="language">${listLanguage}</li>
+                  <li class="list-group-item" id="category">${listCategory}</li>
+                  <li class="list-group-item" id="genre">${listGenre}</li>
+                </ul>
+              </div>
+            </div>
+            `
+  }).join('');
+  container.innerHTML = html;
+  window.scrollTo({
+    top: 1000,
+    left: 0,
+    behavior: 'smooth'
+  });
+}
+
+searchForm.addEventListener('click', () => {
+  searchForm.value = '';
+});
